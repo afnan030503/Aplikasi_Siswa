937a3728e835c31cf9fc1b248c1dfa7761d1d254
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-29 13:24:08 +0700

    membuat fungsi HomeScreendan HomeStatus

---
+++ app/src/main/java/com/example/aplikasisiswa/view/HomeView.kt
@@ -0,0 +1,101 @@
+package com.example.aplikasisiswa.view
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.aplikasisiswa.cmwidget.CostumeTopAppBar
+import com.example.aplikasisiswa.navigasi.DestinasiNavigasi
+import com.example.aplikasisiswa.viewmodel.HomeViewModel
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import com.example.aplikasisiswa.model.Mahasiswa
+import com.example.aplikasisiswa.viewmodel.HomeUiState
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
+@OptIn(ExperimentalMaterial3Api :: class)
+@Composable
+fun HomeScreen(
+    navigateToltemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = (,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+    modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+    topBar = {
+        CostumeTopAppBar(
+            title = DestinasiHome.titleRes,
+            canNavigateBack = false,
+            scrollBehavior = scrollBehavior,
+            onRefresh = {
+                viewModel.getMhs()
+            }
+
+        )
+    },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToltemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Kontak")
+            }
+        }
+    ) { innerPadding ->
+        HomeStatus(
+            homeUiState = viewModel.mhsUIState,
+            retryAction = { viewModel.getMhs() }, modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick, onDeleteClick = {
+                viewModel.deleteMhs(it.nim)
+                viewModel.getMhs()
+            }
+        )
+    }
+}
+
+@Composable
+fun HomeStatus(
+    homeUiState: HomeUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Mahasiswa) -> Unit = {},
+    onDetailClick: (String) -> Unit
+){
+    when (homeUiState) {
+        is HomeUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+
+        is HomeUiState.Success ->
+            if (homeUiState.mahasiswa.isEmpty())
+                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Kontak")
+                }
+            )else {
+        MhsLayout(
+            mahasiswa = homeUiState.mahasiswa, modifier = modifier.fillMaxWidth(),
+            onDetailClick = {
+                onDeleteClick(it.nim)
+            }
+        )
+    }
+    is HomeUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
